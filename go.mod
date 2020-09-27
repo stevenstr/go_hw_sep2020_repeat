09730fbe3ab79a603e1a53e3279def9c49a205ba
Ste Zha <stephenzhavrid@gmail.com>
2020-09-27 19:49:43 +0300

    Hw4.3gomod
    Home work 4.3 Go Module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module module
+
+go 1.13
+
+require github.com/kyokomi/emoji v2.2.4+incompatible
---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+* Author: Stefan
+* Last changes: 09.27.2020
+* Task: Implement a HelloWorld program that will print a smiley using a go module
+ */
+
+package main
+
+import "github.com/kyokomi/emoji"
+
+func main() {
+	emoji.Println("Hello Golang World :smile:!")
+}
